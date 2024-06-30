472f7d5dc843bea211a96a9b46e4faa5e2aef7f9
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-07-01 01:26:01 +0900

    [Silver IV] Title: 정수 제곱근, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+left = 0
+right = n
+
+while left<=right:
+    middle = (left+right)//2
+    if middle**2 < n:
+        left = middle + 1
+    else:
+        right = middle - 1
+
+print(left)
