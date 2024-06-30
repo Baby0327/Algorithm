eea7dc21b694740567adb3ba2974bbf78fe3df5e
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-07-01 00:25:06 +0900

    [Silver V] Title: 단어 정렬, Time: 92 ms, Memory: 35868 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,14 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+word = []
+for i in range(N):
+    word.append(sys.stdin.readline().rstrip())
+
+word = set(word)
+word = list(word)
+word.sort(key=lambda x: (len(x), x))
+
+for i in word:
+    print(i)
