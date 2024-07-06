6e8380d27816d5db46b985ff75480cc45d0f363d
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-07-06 14:07:32 +0900

    [Silver II] Title: N과 M (9), Time: 100 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -10,13 +10,18 @@ N과 M (9)
 def solution():
     # 길이 m의 수열이 완성되었다면, 리스트에서 unpacking하여 출력
     if len(result) == m:
-        temp.append([*result])
+        print(*result)
         return
 
+    # 중복 처리를 위한 변수 선언
+    check = 0
+
     for i in range(1, n+1):
-        if not visited[i]:
+        # 같은 값을 가진 원소에 대한 중복 처리
+        if not visited[i] and check != num[i-1]:
             visited[i] = True
             result.append(num[i-1])
+            check = num[i-1]
             # 다음 원소에 대한 재귀호출
             solution()
             # 이전 상태로 복구
@@ -25,16 +30,9 @@ def solution():
 
 
 n, m = map(int, input().split())
-num = list(map(int, input().split()))
+num = sorted(list(map(int, input().split())))
 visited = [False for i in range(n+1)]
 visited[0] = True
 result = []
-temp = []
 
 solution()
-
-# list는 mutable하기에 hsahable하지 않으므로 tuple형으로 변환하여 처리
-output = sorted(list(set([tuple(i) for i in temp])))
-
-for i in output:
-    print(*i)
