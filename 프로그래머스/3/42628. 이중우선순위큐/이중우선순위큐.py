3f9546eed1e7b724dd47088fccb006ddb25c1c7c
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-07-06 15:27:20 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.06 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,29 @@
+import heapq
+
+def solution(operations):
+    # 최대 힙, 최소 힙 구현 (기본은 최소 힙, 최대 힙은 각 원소에 -를 붙여 구현)
+    maxi = []
+    mini = []
+
+    for i in operations:
+        # 최대 힙에서 최댓값을 pop하고, -를 붙인 값을 최소 힙에서 삭제
+        if i == "D 1" :
+            if maxi and mini:
+                num = heapq.heappop(maxi)
+                mini.remove(-num)
+                # remove로 깨진 힙 재구조화
+                heapq.heapify(mini)
+        # 최소 힙에서 최솟값을 pop하고, -를 붙인 값을 최소 힙에서 삭제
+        elif i == "D -1":
+            if maxi and mini:
+                num = heapq.heappop(mini)
+                maxi.remove(-num)
+                # remove로 깨진 힙 재구조화
+                heapq.heapify(maxi)
+        # 최소 힙에 push, 최대 힙에 - 붙인 값을 push
+        else:
+            heapq.heappush(maxi, -int(i[2:]))
+            heapq.heappush(mini, int(i[2:]))
+            
+    # 힙이 비었으면 [0, 0], 그렇지 않으면 각 힙에서 최댓값과 최솟값을 찾아 반환
+    return [-maxi[0], mini[0]] if maxi and mini else [0, 0]
