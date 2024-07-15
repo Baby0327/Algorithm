88bb57867d2d9e5950b55cc31261c71b2ceff22f
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-07-15 22:26:48 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.py
@@ -0,0 +1,13 @@
+"""
+Celebrity jeopardy
+
+알게 된 점 !!!
+    - exit는 코드 강제 종료에 활용
+    - exit(1) 사용 시 런타임 에러 등 발생할 수 있음
+"""
+
+while True:
+    try:
+        print(input())
+    except:
+        break
