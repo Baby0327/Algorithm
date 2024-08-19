211bcbf682cf8558e51e040a45bd952df947e675
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-08-19 22:25:27 +0900

    [Bronze III] Title: 다음수, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4880. 다음수/다음수.py
@@ -0,0 +1,14 @@
+"""
+다음수
+"""
+
+while True:
+    a, b, c = map(int, input().split())
+
+    if a == b == c == 0:
+        break
+
+    if b-a == c-b:
+        print("AP", c + (c-b))
+    else:
+        print("GP", c * (c//b))
