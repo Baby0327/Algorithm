16f2a517309b897d9d0a1bcd04133e165a12113e
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-08-27 17:17:27 +0900

    [Bronze III] Title: 얼마?, Time: 1388 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9325. 얼마？/얼마？.py
@@ -0,0 +1,15 @@
+"""
+얼마
+"""
+
+t = int(input())
+
+for i in range(t):
+    s = int(input())
+    n = int(input())
+
+    for j in range(n):
+        q, p = map(int, input().split())
+        s += q * p
+
+    print(s)
