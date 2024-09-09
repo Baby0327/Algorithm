fe95a7a36867115c0a0306b70482bae5cda6f7a7
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-09-09 16:55:15 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 2380 ms, Memory: 42388 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.py
@@ -0,0 +1,20 @@
+"""
+인사성 밝은 곰곰이
+"""
+
+n = int(input())
+emoticon = set()
+result = 0
+
+for i in range(n):
+    s = input()
+
+    if s == "ENTER":
+        result += len(emoticon)
+        emoticon = set()
+    else:
+        emoticon.add(s)
+
+result += len(emoticon)
+
+print(result)
