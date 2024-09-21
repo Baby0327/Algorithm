ca3e9cebf2a4b6ed2bcd596ef2b712f002cf4b7a
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-09-21 21:16:48 +0900

    [Bronze III] Title: 찬반투표, Time: 36 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/27736. 찬반투표/찬반투표.py
@@ -0,0 +1,14 @@
+"""
+찬반투표
+"""
+
+n = int(input())
+vote = list(map(int, input().split()))
+
+if vote.count(0) >= n / 2:
+    print("INVALID")
+else:
+    if vote.count(1) > vote.count(-1):
+        print("APPROVED")
+    else:
+        print("REJECTED")
