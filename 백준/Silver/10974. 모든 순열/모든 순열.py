743a8439a7a96d719d33c66630593f9aedb2da33
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-09-22 15:59:25 +0900

    [Silver III] Title: 모든 순열, Time: 176 ms, Memory: 36896 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.py
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -2,9 +2,9 @@
 모든 순열
 
 !!! 알게 된 점 !!!
-    - L10에서  list와 list[:]의 차이를 잊어 오류 발생
+    - L12에서  list와 list[:]의 차이를 잊어 오류 발생
     - list는 값이 계속 변하기에 참조만 추가할 경우 그에 따라 값도 변하는 현상이 발생함
-    - 이를 예방하기 위해 참조인 list가 아닌 값 그 자체인 list[:](복사본)을 저장해야 함
+    - 이를 예방하기 위해 참조인 list가 아닌 값인 list[:](복사본)을 저장해야 함
 """
 
 def back():
@@ -25,7 +25,6 @@ n = int(input())
 visited = [False] * (n + 1)
 list = []
 back()
-result.sort()
 
-for i in result:
+for i in sorted(result):
     print(*i)
