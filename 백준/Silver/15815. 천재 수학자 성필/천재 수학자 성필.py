2f787ffb3a8c7587c309be4630704bdd66aa97f0
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-09-28 13:26:39 +0900

    [Silver III] Title: 천재 수학자 성필, Time: 48 ms, Memory: 34068 KB -BaekjoonHub

--- 백준/Silver/15815. 천재 수학자 성필/천재 수학자 성필.py
+++ 백준/Silver/15815. 천재 수학자 성필/천재 수학자 성필.py
@@ -8,12 +8,22 @@ s = input()
 process = deque()
 
 for i in s:
-    if i in "+-*/":
-        b = str(process.pop())
-        a = str(process.pop())
-        if i == "/":
-            i += "/"    # 모든 결과값은 정수이기에 / 가 아닌 //로 변환
-        process.append(eval(a + i + b))     # eval은 보안에 취약하기에 권장하지 않음
+    if i == "+":
+        b = int(process.pop())
+        a = int(process.pop())
+        process.append(a + b)
+    elif i == "-":
+        b = int(process.pop())
+        a = int(process.pop())
+        process.append(a - b)
+    elif i == "*":
+        b = int(process.pop())
+        a = int(process.pop())
+        process.append(a * b)
+    elif i == "/":
+        b = int(process.pop())
+        a = int(process.pop())
+        process.append(a // b)
     else:
         process.append(i)
 
