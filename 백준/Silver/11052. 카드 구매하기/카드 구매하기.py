84b92d791478dd624982b296d56020007a8b9de8
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-10-06 16:41:26 +0900

    [Silver I] Title: 카드 구매하기, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -10,7 +10,6 @@ card = [0] + list(map(int, input().split()))
 dp = [0] * (n + 1)
 
 for i in range(1, n + 1):
-    for j in range(1, i + 1):
-        dp[i] = max(dp[i], card[j] + dp[i - j])
+    dp[i] = max(dp[i], * [card[j] + dp[i - j] for j in range(1, i + 1)])
 
 print(dp[-1])
