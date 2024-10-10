10d68e012d2ae50c3e10ace1c3d3259a20afc2ac
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-10-10 16:17:30 +0900

    [Gold IV] Title: 동전 1, Time: 160 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -6,15 +6,12 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-v = []
 dp = [0] * (k + 1)
 dp[0] = 1
 
 for i in range(n):
-    v.append(int(input()))
-
-for i in v:
-    for j in range(i, k + 1):
-        dp[j] += dp[j - i]
+    coin = int(input())
+    for j in range(coin, k + 1):
+        dp[j] += dp[j - coin]
 
 print(dp[-1])
