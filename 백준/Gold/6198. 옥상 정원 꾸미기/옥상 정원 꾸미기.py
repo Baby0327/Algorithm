4193a2dc1f6bc072ecb29d62dafad3d7891c58eb
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-10-12 16:27:06 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 80 ms, Memory: 33860 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
@@ -2,20 +2,17 @@
 옥상 정원 꾸미기
 """
 
-from collections import deque
 import sys
 input = sys.stdin.readline
 
-building = deque()
+building = []
 count = 0
 
 for i in range(int(input())):
     now = int(input())
 
     while building:
-        if building[0] <= now:
-            building.popleft()
-        elif building[-1] <= now:
+        if building[-1] <= now:
             building.pop()
         else:
             break
