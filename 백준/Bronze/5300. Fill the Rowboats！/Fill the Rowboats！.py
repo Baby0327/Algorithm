d8e22fb9be66e43cafc9c31325918b2adfb687ae
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-10-16 22:35:12 +0900

    [Bronze IV] Title: Fill the Rowboats!, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5300. Fill the Rowboats！/Fill the Rowboats！.py
@@ -0,0 +1,13 @@
+"""
+Fill the Rowboats!
+"""
+
+n = int(input())
+
+for i in range(n):
+    print(i + 1, end=" ")
+    if (i + 1) % 6 == 0:
+        print("Go!", end=" ")
+
+if n % 6 != 0:
+    print("Go!")
