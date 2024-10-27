c9f6a99aaaead15ca24a11933c59081f852ae3d5
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-10-27 16:29:49 +0900

    [level 0] Title: OX퀴즈, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -1,3 +1,17 @@
 def solution(quiz):
-    answer = ["O" if eval(i.replace("=", "==")) else "X" for i in quiz]
+    answer = []
+    
+    for i in quiz:
+        now = i.split()
+        if now[1] == "+":
+            if int(now[0]) + int(now[2]) == int(now[4]):
+                answer.append("O")
+            else:
+                answer.append("X")
+        else:
+            if int(now[0]) - int(now[2]) == int(now[4]):
+                answer.append("O")
+            else:
+                answer.append("X")
+        
     return answer
