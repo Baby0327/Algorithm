a1fff3ba560833754f43bd362bd728690c3f9bad
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-10-28 18:53:02 +0900

    [Bronze IV] Title: 빵, Time: 32 ms, Memory: 31120 KB, Score: 33 point -BaekjoonHub

--- 백준/Bronze/25377. 빵/빵.py
+++ 백준/Bronze/25377. 빵/빵.py
@@ -2,7 +2,7 @@ result = 1000
 
 for i in range(int(input())):
     a, b = map(int, input().split())
-    if a < b and b < result:
+    if a <= b and b < result:
         result = b
 
 print(result if result != 1000 else -1)
