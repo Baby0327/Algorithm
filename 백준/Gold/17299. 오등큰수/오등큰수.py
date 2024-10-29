21703e13502ad16332ff3d72e026aacd4d87199a
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-10-29 22:41:28 +0900

    [Gold III] Title: 오등큰수, Time: 1476 ms, Memory: 155544 KB -BaekjoonHub

--- 백준/Gold/17299. 오등큰수/오등큰수.py
+++ 백준/Gold/17299. 오등큰수/오등큰수.py
@@ -5,10 +5,10 @@ n = int(input())
 a = list(map(int, input().split()))
 result = [-1] * n
 stack = []
-count = {}
+count = [0] * 1000001
 
 for i in a:
-    count[i] = count.get(i, 0) + 1
+    count[i] += 1
 
 for i in range(n):
     while stack and count[a[stack[-1]]] < count[a[i]]:
