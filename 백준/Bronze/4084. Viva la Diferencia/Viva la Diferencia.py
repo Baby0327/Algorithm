32e55be1e9ad8aa7e84a53251e096d7c564f858d
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-11-02 21:07:22 +0900

    [Bronze III] Title: Viva la Diferencia, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4084. Viva la Diferencia/Viva la Diferencia.py
+++ 백준/Bronze/4084. Viva la Diferencia/Viva la Diferencia.py
@@ -1,15 +1,15 @@
+import sys
+input = sys.stdin.readline
+
 while True:
-    num = list(map(int, input().split()))
+    a, b, c, d = map(int, input().split())
     cnt = 0
 
-    if num[0] == num[1] == num[2] == num[3] == 0:
+    if a == b == c == d == 0:
         break
 
-    while num.count(num[0]) != 4:
-        temp = []
-        for i in range(-4, 0):
-            temp.append(abs(num[i] - num[i + 1]))
-        num = temp
+    while not (a == b == c == d):
+        a, b, c, d = map(abs, (a - b, b - c, c - d, d - a))
         cnt += 1
 
     print(cnt)
