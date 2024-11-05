620bef812f007d395f5e58f5d471ce5dea5f22fb
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-11-05 20:59:53 +0900

    [Silver V] Title: 콰트로치즈피자, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/27964. 콰트로치즈피자/콰트로치즈피자.py
+++ 백준/Silver/27964. 콰트로치즈피자/콰트로치즈피자.py
@@ -1,8 +1,2 @@
 n = int(input())
-tp = set()
-
-for i in input().split():
-    if i.endswith("Cheese"):
-        tp.add(i)
-
-print("yummy" if len(tp) >= 4 else "sad")
+print("yummy" if len(set(i for i in input().split() if i.endswith("Cheese"))) >= 4 else "sad")
