edd208093a93a37a3a6a436951ab107233da625b
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-11-06 20:40:22 +0900

    [Gold V] Title: LCS, Time: 196 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -3,13 +3,14 @@ input = sys.stdin.readline
 
 s1 = input().rstrip()
 s2 = input().rstrip()
-dp = [[0] * (len(s1) + 1) for _ in range(len(s2) + 1)]
+dp = [0] * len(s2)
 
-for i in range(1, len(s2) + 1):
-    for j in range(1, len(s1) + 1):
-        if s2[i - 1] == s1[j - 1]:
-            dp[i][j] = dp[i - 1][j - 1] + 1
-        else:
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+for i in range(len(s1)):
+    temp = 0
+    for j in range(len(s2)):
+        if temp < dp[j]:
+            temp = dp[j]
+        elif s1[i] == s2[j]:
+            dp[j] = temp + 1
 
-print(dp[-1][-1])
+print(max(dp))
