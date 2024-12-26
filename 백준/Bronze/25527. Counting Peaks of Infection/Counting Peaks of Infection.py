09fe74d1dc68dd421616810140c05214c88908b5
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2024-12-26 12:29:53 +0900

    [Bronze III] Title: Counting Peaks of Infection, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25527. Counting Peaks of Infection/Counting Peaks of Infection.py
@@ -0,0 +1,14 @@
+while 1:
+    n = int(input())
+
+    if n == 0:
+        break
+
+    v = list(map(int, input().split()))
+    cnt = 0
+
+    for i in range(1, n - 1):
+        if v[i - 1] < v[i] and v[i + 1] < v[i]:
+            cnt += 1
+
+    print(cnt)
