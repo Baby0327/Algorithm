8e4afb04b4774a9061f6137e0536fbcfeb91a7e7
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-04-18 02:18:47 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 9.05 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.py
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -1,6 +1,9 @@
 def solution(angle):
-    if angle <= 90:
-        answer = 1 + int(angle == 90)
+    if angle < 90:
+        return 1
+    elif angle == 90:
+        return 2
+    elif angle < 180:
+        return 3
     else:
-        answer = 3 + int(angle == 180)
-    return answer
+        return 4
