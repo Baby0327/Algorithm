1b020743fc8319eab293e026575beeb741deef56
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-04-18 02:14:21 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,9 +1,7 @@
 def solution(id_pw, db):
-    answer = "fail"
-    for id, pw in db:
-        if id_pw[0] == id:
-            answer = "wrong pw"
-            if id_pw[1] == pw:
-                answer = "login"
-                break
-    return answer
+    if id_pw in db:
+        return "login"
+    elif id_pw[0] in [i[0] for i in db]:
+        return "wrong pw"
+    else:
+        return "fail"
