1bd60cd996539548bb4a90c0a8284b6c8e4c4291
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-04-18 02:09:02 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.18 ms, Memory: 8.94 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,10 +1,3 @@
 def solution(array):
-    dic = {}
-    
-    for i in array:
-        dic[i] = dic.get(i, 0) + 1
-    
-    order = sorted(list(dic.items()), key=lambda x : -x[1])
-    print(order)
-    
-    return -1 if len(order) > 1 and order[0][1] == order[1][1] else order[0][0]
+    cnt = sorted([[array.count(i), i] for i in set(array)], reverse = True)
+    return -1 if len(cnt) > 1 and cnt[0][0] == cnt[1][0] else cnt[0][1]
