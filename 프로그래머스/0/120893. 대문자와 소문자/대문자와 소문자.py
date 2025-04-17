4e595da86a11be30c0884899dbb170a33a14a5db
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-04-17 22:45:36 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.15 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,3 +1,2 @@
 def solution(my_string):
-    answer = "".join(i.upper() if i.islower() else i.lower() for i in my_string)
-    return answer
+    return "".join([i.upper() if i.islower() else i.lower() for i in my_string])
