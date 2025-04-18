2a51802fbd849dd6d091a46b9f1baa0cbcbe970a
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-04-18 23:37:47 +0900

    [level 0] Title: A로 B 만들기, Time: 0.37 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
@@ -1,7 +1,2 @@
 def solution(before, after):
-    answer = 1
-    for i in after:
-        if before.count(i) != after.count(i):
-            answer = 0
-            break
-    return answer
+    return int(sorted(before) == sorted(after))
