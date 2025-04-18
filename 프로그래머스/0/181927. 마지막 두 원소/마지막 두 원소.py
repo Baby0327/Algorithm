929947c19e3bbfe317d0fc75254301056d1be74e
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-04-18 18:43:26 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,4 +1,2 @@
 def solution(num_list):
-    num_list.append(num_list[-1] - num_list[-2] if num_list[-1] > num_list[-2] else num_list[-1] * 2 )
-    answer = num_list
-    return answer
+    return num_list + ([num_list[-1] - num_list[-2]] if num_list[-1] > num_list[-2] else [num_list[-1] * 2])
