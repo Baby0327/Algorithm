cf9eb2fb5d827c4996da114b540fa18af91bec51
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-04-18 14:17:59 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 16.46 ms, Memory: 7.35 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
@@ -1,8 +1,2 @@
 str = input()
-answer = ""
-for i in str:
-    if ord(i) >= 97:
-        answer += chr(ord(i)-32)
-    else:
-        answer += chr(ord(i) + 32)
-print(answer)
+print(str.swapcase())
