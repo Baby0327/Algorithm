e1941899f71d94f9b4ed8f29d354e68118b342ab
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-04-19 00:23:38 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.04 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,16 +1,17 @@
 def solution(numbers):
-    number = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
-    answer = ""
+    english = {"zero" : 0, "one" : 1, "two" : 2, "three" : 3, "four" : 4, "five" : 5, "six" : 6, "seven" : 7, "eight" : 8, "nine" : 9, "ten" : 10}
+    answer = []
     i = 3
     
     while numbers:
-        word = numbers[:i]
+        now = numbers[:i]
         
-        if word in number:
-            answer += str(number.index(word))
+        if now in english:
+            answer.append(english[now])
             numbers = numbers[i:]
             i = 3
         else:
             i += 1
-            
-    return int(answer)
+    
+    
+    return int("".join(map(str, answer)))
