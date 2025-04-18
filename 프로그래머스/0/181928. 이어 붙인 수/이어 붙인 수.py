b56f782902774b6cce6cb6129e40cef968ea3a3e
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-04-18 18:37:56 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.04 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -1,10 +1,2 @@
 def solution(num_list):
-    even = ""
-    odd = ""
-    for i in num_list:
-        if i % 2 == 0:
-            even += str(i)
-        else:
-            odd += str(i)
-    answer = int(even) + int(odd)
-    return answer
+    return int("".join(str(num_list[i]) for i in range(len(num_list)) if num_list[i] % 2)) + int("".join(str(num_list[i]) for i in range(len(num_list)) if num_list[i] % 2 == 0))
