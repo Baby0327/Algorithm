b8d186f268af8c3b1aef715aff40c2cd68e5751d
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-04-18 17:15:35 +0900

    [level 0] Title: 글자 지우기, Time: 0.01 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
@@ -1,6 +1,7 @@
 def solution(my_string, indices):
-    my_string = list(my_string)
+    answer = list(my_string)
+    
     for i in indices:
-        my_string[i] = ""
-    answer = "".join(my_string)
-    return answer
+        answer[i] = ""
+    
+    return "".join(answer)
