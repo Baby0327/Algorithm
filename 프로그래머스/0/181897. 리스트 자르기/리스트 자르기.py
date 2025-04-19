d52267e313a5bfdd498fded45ce8761feb787800
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-04-19 17:07:44 +0900

    [level 0] Title: 리스트 자르기, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.py
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.py
@@ -1,10 +1,11 @@
 def solution(n, slicer, num_list):
+    a, b, c = slicer
+    
     if n == 1:
-        answer = num_list[:slicer[1]+1]
+        return num_list[:b + 1]
     elif n == 2:
-        answer = num_list[slicer[0]:]
+        return num_list[a:]
     elif n == 3:
-        answer = num_list[slicer[0]:slicer[1]+1]
-    elif n == 4:
-        answer = num_list[slicer[0]:slicer[1]+1:slicer[2]]
-    return answer
+        return num_list[a:b + 1]
+    else:
+        return num_list[a:b + 1:c]
