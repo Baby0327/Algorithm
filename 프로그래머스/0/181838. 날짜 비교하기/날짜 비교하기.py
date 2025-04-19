24a8cdb3e2d056e3362d9157fdca420abe023b1c
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-04-19 17:21:50 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.01 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,3 +1,5 @@
 def solution(date1, date2):
-    answer = int(int("".join(map(str, date1))) < int("".join(map(str, date2))))
-    return answer
+    day1 = sum(date1[i] * j for i, j in zip([0, 1, 2], [365, 30, 1]))
+    day2 = sum(date2[i] * j for i, j in zip([0, 1, 2], [365, 30, 1]))
+    
+    return int(day1 < day2)
