fa6bb9845ae9c2d42cf9367f5eeeccb5a337caa4
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-04-23 15:25:24 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -1,10 +1,8 @@
 def solution(str_list):
-    answer = []
     for i in range(len(str_list)):
-        if str_list[i] == 'l':
-            answer = str_list[:i]
-            break
-        elif str_list[i] == 'r':
-            answer = str_list[i+1:]
-            break
-    return answer
+        if str_list[i] == "l":
+            return str_list[:i]
+        elif str_list[i] == "r":
+            return str_list[i + 1:]
+    
+    return []
