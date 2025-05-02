e028f61c4c0a6689bd9e90779ed2ca979414c9c6
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-05-02 18:30:24 +0900

    [level 0] Title: l로 만들기, Time: 10.48 ms, Memory: 9.89 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -1,3 +1,2 @@
 def solution(myString):
-    answer = ''.join([i if ord(i) > ord('l') else 'l' for i in myString])
-    return answer
+    return "".join([i if ord(i) > ord("l") else "l" for i in myString])
