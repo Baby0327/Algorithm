dac0bd155a03fc95aa0d956c4aaad3872f070961
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-05-07 22:24:54 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 15.56 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -1,6 +1,12 @@
 def solution(arr, queries):
     answer = []
+    
     for s, e, k in queries:
-        tmp = sorted([i for i in arr[s:e+1] if i > k])
-        answer.append(-1 if len(tmp) == 0 else tmp[0])
+        num = sorted([arr[i] for i in range(s, e + 1) if arr[i] > k])
+        
+        if num:
+            answer.append(num[0])
+        else:
+            answer.append(-1)
+        
     return answer
