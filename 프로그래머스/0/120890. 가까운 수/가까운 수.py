57cc1a19a3a30a0b09166e9e588e5eee28b93976
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-06-10 22:23:55 +0900

    [level 0] Title: 가까운 수, Time: 0.04 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -1,5 +1,2 @@
 def solution(array, n):
-    array.append(n)
-    array.sort()
-    answer = sorted([array[array.index(n)-1], array[(array.index(n) + 1)%len(array)]], key=lambda x:abs(x-n))
-    return answer[0]
+    return sorted([[i - n, i] for i in array], key=lambda x : (abs(x[0]), x[0]))[0][1]
