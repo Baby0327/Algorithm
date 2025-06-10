50197d0e18f4067892d22e030c281e4ee1510c52
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-06-10 22:16:39 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,5 +1,7 @@
 def solution(my_string, num1, num2):
-    answer = list(my_string)
-    answer[num1], answer[num2] = answer[num2], answer[num1]
-    answer = "".join(answer)
-    return answer
+    my_string = list(my_string)
+    temp = my_string[num1]
+    my_string[num1] = my_string[num2]
+    my_string[num2] = temp
+    
+    return "".join(my_string)
