57cf729738ddd748a37f7d587d069003c7d25eb1
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-06-11 21:24:36 +0900

    [level 0] Title: 배열 만들기 2, Time: 1393.55 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,6 +1,10 @@
 def solution(l, r):
     answer = []
-    for i in range(l, r+1):
-        if str(i).count('5') + str(i).count('0') == len(str(i)):
+    
+    for i in range(l, r + 1):
+        num = set(map(int, list(str(i))))
+        
+        if num - {0, 5} == set():
             answer.append(i)
+    
     return answer or [-1]
