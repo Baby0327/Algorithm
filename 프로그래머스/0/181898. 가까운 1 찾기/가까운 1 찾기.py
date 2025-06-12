d8134d5a5bff65197205cac8ad43935ded6c5314
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-06-12 14:53:28 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.01 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -1,7 +1,6 @@
 def solution(arr, idx):
-    answer = -1
     for i in range(idx, len(arr)):
-        if arr[i] == 1:
-            answer = i
-            break
-    return answer
+        if arr[i]:
+            return i
+        
+    return -1
