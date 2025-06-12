5c332843219db1f5f9a0aaf450459d712c64a324
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-06-12 16:02:16 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -1,7 +1,13 @@
 def solution(num_list):
     answer = 0
-    for i in num_list:
-        while i != 1:
-            i //= 2
+    
+    for num in num_list:
+        while num != 1:
+            if num % 2:
+                num = (num - 1) // 2
+            else:
+                num //= 2
+            
             answer += 1
+    
     return answer
