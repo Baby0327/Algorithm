07c350db25510e6b51ad9f7c22e5e775906b1800
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-06-23 13:06:14 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,8 +1,9 @@
 def solution(a, b, c):
-    test = sorted([a, b, c])
-    answer = a + b + c
-    if test[0] == test[1] or test[1] == test[2]:
-        answer *= (a**2 + b**2 + c**2)
-        if test[0] == test[2]:
-            answer *= (a**3 + b**3 + c**3)
-    return answer
+    num = sorted([a, b, c])
+    
+    if num[0] == num[2]:
+        return (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
+    elif num[0] == num[1] or num[1] == num[2]:
+        return (a + b + c) * (a**2 + b**2 + c**2)
+    else:
+        return a + b + c
