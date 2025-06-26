650f80663b6c923dfae8db15db826c0f7db243fd
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2025-06-26 15:45:05 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
@@ -1,8 +1,13 @@
 def solution(spell, dic):
-    for i in dic:
-        check = []
-        for j in spell:
-            check.append(i.count(j))
-        if len(check) == check.count(1):
+    for word in dic:
+        check = True
+        
+        for s in spell:
+            if word.count(s) != 1:
+                check = False
+                break
+        
+        if check:
             return 1
+        
     return 2
