bd1d0ba6f8dddce3e21b7b373bb08c63818a4f69
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2026-02-23 20:58:31 +0900

    [Bronze III] Title: 수학적 호기심, Time: 184 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
@@ -1,13 +1,16 @@
 import sys
 input = sys.stdin.readline
 
+p = [[0 for i in range(101)] for j in range(101)]
+
+for b in range(2, 101):
+    for m in range(1, 101):
+        p[b][m] = p[b - 1][m]
+
+        for a in range(1, b):
+            if ((a**2 + b**2 + m) / (a * b)).is_integer():
+                p[b][m] += 1
+
 for _ in range(int(input())):
     n, m = map(int, input().split())
-    cnt = 0
-
-    for b in range(2, n):
-        for a in range(1, b):
-            if ((a**2 + b**2 + m) / (a*b)).is_integer():
-                cnt += 1
-
-    print(cnt)
+    print(p[n - 1][m])
