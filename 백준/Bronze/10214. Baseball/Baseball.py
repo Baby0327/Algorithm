823ace00067d8d16a7eb918647154dfc87497023
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2026-02-24 20:16:33 +0900

    [Bronze III] Title: Baseball, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10214. Baseball/Baseball.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    y, k = 0, 0
+
+    for i in range(9):
+        a, b = map(int, input().split())
+        y += a
+        k += b
+
+    if y > k:
+        print("Yonsei")
+    elif y < k:
+        print("Korea")
+    else:
+        print("Draw")
