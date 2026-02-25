721539de93e10addd76ec857e95a5049da82a537
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2026-02-25 20:35:15 +0900

    [Bronze III] Title: 캥거루 세마리2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11034. 캥거루 세마리2/캥거루 세마리2.py
+++ 백준/Bronze/11034. 캥거루 세마리2/캥거루 세마리2.py
@@ -1,15 +1,9 @@
+import sys
+input = sys.stdin.readline
+
 while 1:
     try:
         a, b, c = map(int, input().split())
-
-        if b - a < c - b:
-            print( c - b - 1)
-        elif b - a > c - b:
-            print(b - a - 1)
-        elif  b - a > 0:
-            print(b - a - 1)
-        else:
-            print(0)
-
-    except EOFError:
+        print(max(b - a, c - b) - 1)
+    except:
         break
