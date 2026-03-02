645c9f1d3aa490cec43c778f2a669e0eab46201f
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2026-03-02 11:40:56 +0900

    [Bronze IV] Title: Archivist, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.py
@@ -0,0 +1,4 @@
+w = ["ITMO", "SPbSU", "SPbSU", "ITMO", "ITMO", "SPbSU", "ITMO", "ITMO", "ITMO", "ITMO", "ITMO",
+        "PetrSU, ITMO", "SPbSU", "SPbSU", "ITMO", "ITMO", "ITMO", "ITMO", "SPbSU", "ITMO", "ITMO",
+        "ITMO", "ITMO", "SPbSU", "ITMO"]
+print(w[int(input()) - 1995])
