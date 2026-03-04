d65f95764b41916e64d1854ad8a3785133af8cae
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2026-03-04 21:30:58 +0900

    [Bronze III] Title: 꿍의 우주여행, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9501. 꿍의 우주여행/꿍의 우주여행.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n, d = map(int, input().split())
+    cnt = 0
+
+    for i in range(n):
+        v, f, c = map(int, input().split())
+
+        if v * (f / c) >= d:
+            cnt += 1
+
+    print(cnt)
