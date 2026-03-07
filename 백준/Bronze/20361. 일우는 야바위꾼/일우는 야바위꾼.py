b8707517f32fed09b8d8b81b546a210600455535
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2026-03-07 16:40:32 +0900

    [Bronze III] Title: 일우는 야바위꾼, Time: 104 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/20361. 일우는 야바위꾼/일우는 야바위꾼.py
+++ 백준/Bronze/20361. 일우는 야바위꾼/일우는 야바위꾼.py
@@ -2,11 +2,13 @@ import sys
 input = sys.stdin.readline
 
 n, x, k = map(int, input().split())
-b = [0] * (n + 1)
-b[x] = 1
 
 for _ in range(k):
     i, j = map(int, input().split())
-    b[i], b[j] = b[j], b[i]
 
-print(b.index(1))
+    if i == x:
+        x = j
+    elif j == x:
+        x = i
+
+print(x)
