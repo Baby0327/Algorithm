0e8bfaa2e592277282796221d735479f6c8d496c
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2026-03-13 09:38:20 +0900

    [Silver IV] Title: ∑|ΔEasyMAX|, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/17203. ∑｜ΔEasyMAX｜/∑｜ΔEasyMAX｜.py
+++ 백준/Silver/17203. ∑｜ΔEasyMAX｜/∑｜ΔEasyMAX｜.py
@@ -1,11 +1,10 @@
 n, q = map(int, input().split())
 a = list(map(int, input().split()))
+s = [0]
+
+for i in range(n - 1):
+    s.append(s[-1] + abs(a[i + 1] - a[i]))
 
 for _ in range(q):
     l, r = map(int, input().split())
-    s = 0
-
-    for i in range(l, r):
-        s += abs(a[i - 1] - a[i])
-
-    print(s)
+    print(s[r - 1] - s[l - 1])
