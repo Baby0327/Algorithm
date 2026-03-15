895ee698cccedab8392dc9578b08e4f291340d25
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2026-03-16 08:19:00 +0900

    [Silver V] Title: 마트료시카 합치기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/25631. 마트료시카 합치기/마트료시카 합치기.py
+++ 백준/Silver/25631. 마트료시카 합치기/마트료시카 합치기.py
@@ -1,18 +1,4 @@
 n = int(input())
-a = sorted(list(map(int, input().split())), reverse=True)
-cnt = []
-
-for i in a:
-    if cnt:
-        check = 1
-        for j in range(len(cnt)):
-            if i < cnt[j]:
-                cnt[j] = i
-                check = 0
-                break
-        if check:
-            cnt.append(i)
-    else:
-        cnt.append(i)
-
-print(len(cnt))
+a = list(map(int, input().split()))
+s = set(a)
+print(max(a.count(i) for i in s))
