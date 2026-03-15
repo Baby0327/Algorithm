846d3b70c2c818f9c0cf203602f59d871d50b82d
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2026-03-16 08:38:11 +0900

    [Silver V] Title: 이름 궁합, Time: 844 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15312. 이름 궁합/이름 궁합.py
+++ 백준/Silver/15312. 이름 궁합/이름 궁합.py
@@ -1,12 +1,9 @@
-a = input()
-b = input()
+a = input().strip()
+b = input().strip()
 n = [3, 2, 1, 2, 3, 3, 2, 3, 3, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 2, 1]
 num = [n[ord(b[i // 2]) - 65] if i % 2 else n[ord(a[i // 2]) - 65] for i in range(len(a) * 2)]
 
 while len(num) > 2:
-    temp = []
-    for i in range(len(num) - 1):
-        temp.append((num[i] + num[i + 1]) % 10)
-    num = temp
+    num = [(num[i] + num[i + 1]) % 10 for i in range(len(num) - 1)]
 
 print(*num, sep="")
