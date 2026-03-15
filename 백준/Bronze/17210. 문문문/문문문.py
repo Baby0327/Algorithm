de8e5551b05ed25dbfdea82f71128dce27b09fc4
Baby0327 <167307819+Baby0327@users.noreply.github.com>
2026-03-15 10:06:58 +0900

    [Bronze III] Title: 문문문, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/17210. 문문문/문문문.py
+++ 백준/Bronze/17210. 문문문/문문문.py
@@ -1,12 +1,12 @@
 n = int(input())
-s = [int(input())]
+s = [input()]
 
 if n < 6:
     for i in range(n - 1):
-        if s[-1]:
-            s.append(0)
+        if s[-1] == "1":
+            s.append("0")
         else:
-            s.append(1)
-    print("\n".join(map(str, s[1:])))
+            s.append("1")
+    print("\n".join(s[1:]))
 else:
     print("Love is open door")
